7c4d0bd6dbda8dee780fbeb9146b6392853c37b6
Aditya <modanwaladi@gmail.com>
2023-07-26 06:27:46 -0700

    first commit

---
+++ alpha-1/new.py
@@ -0,0 +1,39 @@
+# x = input('Enter your name here : ')
+
+# if len(x) == 4:
+#     print('You have 4 letters in your name')
+
+# elif len(x) == 0:
+#     print('You typed nothing...')
+
+
+# num = int(input('Enter your num here : '))
+# if num % 2 == 0:
+#     print("Your num is Even")
+# else:
+#     print('Your num is odd')
+
+
+# a, b, c = 30, 50, 23
+
+# if a>b and a>c:
+#     print('a is greater')
+# elif b>a and c>a:
+#     print('B is greater')
+# else:
+#     print('c is greater')
+
+# day = input('Enter day here : ')
+# match (day):
+#     case 'monday':
+#         print('You enter monday')
+#     case  'tuesday':
+#         print('You enter tuesday')
+#     case 'wednesday'|'thursday':
+#         print('You enter a day')
+#     case 'friday':
+#         print('You enter friday')
+#     case _:
+#         print('You chose a wrong day')
+
+
---
+++ alpha-1/ui.py
@@ -0,0 +1,49 @@
+# name =  "priya"
+# print(name,type(name))
+
+
+# age = 21
+# print(age,type(age))
+
+# f = 3.4
+# print(f,type(f))
+
+# b = True
+# print(b,type(b))
+
+
+# n = None
+# print(n,type(n))
+
+
+# a = 30 
+# b =  20  
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a%b)
+# print(a/b)
+# print(a//b)
+ 
+
+# weather = "sunny"
+# if weather  == "ranny"
+# elif
+
+
+
+
+weather =  input('enter weather here:')
+
+if weather == 'rainy':
+    print("its rain outside ")
+elif weather =='cloudy':    
+    print('its clody outside')
+elif weather =='sunny':    
+    print('its sunny outside ')
+else:    
+    print('invalid input')
+    
+
+
+
